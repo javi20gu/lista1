b6420899f6a6b003173fd5e504282f781c278ea0
Javier <javierhidalgo_c@hotmail.com>
2018-01-16 20:04:43 +0100

    Introducimos salida del programa

--- lista.py
+++ lista.py
@@ -1,5 +1,8 @@
-nombre = input("Introduce tu nombre: ")
-for i in nombre:
-	print(i.upper())
-print("Gracias por utilizarlo")
+preguntar = "no".lower()
+while preguntar == "no":
+        nombre = input("Introduce tu nombre: ")
+        for i in nombre:
+                print(i.upper())
+        print("Gracias por utilizarlo")
+        preguntar = input("¿Quieres salirte del programa? si/no \n>>>").lower()
 input("Pulse cualquier tecla para salir...")
