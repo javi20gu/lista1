3a156fe0e07ca86565b198148b65182bdbd3a0e4
Javier <javierhidalgo_c@hotmail.com>
2018-01-16 20:25:42 +0100

    Borrado codigo

--- lista.py
+++ lista.py
@@ -3,6 +3,5 @@ while preguntar == "no":
         nombre = input("Introduce tu nombre: ")
         for i in nombre:
                 print(i.upper())
-        print("Gracias por utilizarlo")
         preguntar = input("¿Quieres salirte del programa? si/no \n>>>").lower()
 input("Pulse cualquier tecla para salir...")
